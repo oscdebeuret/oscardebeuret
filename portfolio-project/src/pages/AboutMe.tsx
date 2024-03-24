2047db99997eeaf9bcb90008e42390ded8813f29
Oscar <debeuret.oscar@gmail.com>
2024-03-24 15:55:56 +0100

    responsive AboutMe

--- portfolio-project/src/pages/AboutMe.tsx
+++ portfolio-project/src/pages/AboutMe.tsx
@@ -9,8 +9,8 @@ function AboutMe() {
             <div className="article-section gradient-blue-tr">
                 <div className="flex flex-col container">
                     <h1 className="underline-purple mb-10" id="pres">Présentation</h1>
-                    <div className="flex md:flex-row md:justify-between md:items-start flex-col align-center justify-center items-center">
-                        <div className="flex flex-col w-7/12 border-2 p-5 py-7 rounded-3xl lg:pe-12 ">
+                    <div className="flex md:flex-row md:justify-between md:items-start flex-col-reverse align-center justify-center items-center">
+                        <div className="flex flex-col w-full md:w-7/12 border-2 p-5 py-7 rounded-3xl lg:pe-12 ">
                             <p className="poppins text-xl text-left font-medium mb-5 leading-8">Étudiant en 3ème année à l’IUT Informatique de Bordeaux, actuellement en alternance chez bewease.</p>
                             <p className="poppins text-xl text-left font-medium leading-8">J’aime monter des <span className="underline-violet px-2">projets</span> informatiques innovants et <span className="underline-violet px-2">créatifs</span>. Découvrez mes projets sur ce portfolio.</p>
                         </div>
@@ -20,7 +20,7 @@ function AboutMe() {
                             </div>
                         </div>
                     </div>
-                    <div className="flex flex-row w-fulljustify-between mt-16">
+                    <div className="flex flex-col md:flex-row w-fulljustify-between mt-16">
                         <Formation></Formation>
                         <Experience></Experience>
                     </div>
@@ -34,30 +34,30 @@ function AboutMe() {
 const Experience = () => {
     return (
         <>
-            <div className="flex flex-col w-6/12 text-left">
+            <div className="flex flex-col w-full md:w-6/12 text-left mb-10 md:mb-0">
                 <h3 className="lexend-giga text-2xl tracking-[-3px] tracking-tighter mb-10">Experience</h3>
                 <div className="flex flex-row">
                     <div className="flex flex-col">
                         <div className="flex w-24 h-24 border-2 rounded-[15px] items-center justify-center">
                             <img src={Bewease} alt="" className="w-16 h-16"></img>
                         </div>
-                        <div className="block w-12 h-24 border-e-2"></div>
+                        <div className="block w-12 h-12 md:h-24 border-e-2"></div>
                         <div className="flex w-24 h-24 border-2 rounded-[15px] items-center justify-center">
                             <img src={Bewease} className="w-16 h-16" />
                         </div>
-                        <div className="block w-12 h-24 border-e-2"></div>
+                        <div className="block w-12 h-12 md:h-24 border-e-2"></div>
                     </div>
-                    <div className="flex flex-col">
+                    <div className="flex grow flex-col">
                         <div className="flex flex-col ms-5">
-                            <p className="poppins font-semibold text-[24px]">Full Stack Developer Alternance</p>
-                            <p className="poppins font-medium text-[20px] text-neutral-400">bewease - Pessac (33)</p>
-                            <p className="poppins font-medium text-[20px] text-neutral-400">juillet 2023 - Aujourd’hui</p>
+                            <p className="poppins font-semibold text-[18px] md:text-[24px]">Full Stack Developer Alternance</p>
+                            <p className="poppins font-medium text-[16px] md:text-[20px] text-neutral-400">bewease - Pessac (33)</p>
+                            <p className="poppins font-medium text-[16px] md:text-[20px] text-neutral-400">juillet 2023 - Aujourd’hui</p>
                         </div>
-                        <div className="h-[60px]"></div>
+                        <div className="h-[50px] md:h-[60px]"></div>
                         <div className="flex flex-col ms-5">
-                            <p className="poppins font-semibold text-[24px]">Full Stack Developer Stagiaire</p>
-                            <p className="poppins font-medium text-[20px] text-neutral-400">bewease - Pessac (33)</p>
-                            <p className="poppins font-medium text-[20px] text-neutral-400">Avril - juin 2023</p>
+                            <p className="poppins font-semibold text-[18px] md:text-[24px]">Full Stack Developer Stagiaire</p>
+                            <p className="poppins font-medium text-[16px] md:text-[20px] text-neutral-400">bewease - Pessac (33)</p>
+                            <p className="poppins font-medium text-[16px] md:text-[20px] text-neutral-400">Avril - juin 2023</p>
                         </div>
                     </div>
                 </div>
@@ -69,32 +69,32 @@ const Experience = () => {
 const Formation = () => {
     return (
         <>
-            <div className="flex flex-col w-7/12 text-left">
+            <div className="flex flex-col w-full md:w-7/12 text-left mb-10 md:mb-0">
                 <h3 className="lexend-giga text-2xl tracking-[-3px] tracking-tighter mb-12">Formation</h3>
                 <div className="flex flex-row">
                     <div className="flex flex-col">
                         <div className="flex w-24 h-24 border-2 rounded-[15px] items-center justify-center">
                             <img src={Ub} alt="" className="w-20 h-20"></img>
                         </div>
-                        <div className="block w-12 h-24 border-e-2"></div>
+                        <div className="block w-12 h-12 md:h-24 border-e-2"></div>
                         <div className="flex w-24 h-24 border-2 rounded-[15px] items-center justify-center">
                             <img src={Daguin} className="w-16 h-16" />
                         </div>
-                        <div className="block w-12 h-24 border-e-2"></div>
+                        <div className="block w-12 h-12 md:h-24 border-e-2"></div>
                     </div>
                     <div className="flex flex-col">
                         <div className="flex flex-col ms-5">
-                            <p className="poppins font-semibold text-[24px]">BUT Informatique</p>
-                            <p className="poppins font-medium text-[20px] text-neutral-400">Université de Bordeaux (33)</p>
-                            <p className="poppins font-medium text-[20px] text-neutral-400">2021 - Aujourd'hui</p>
+                            <p className="poppins font-semibold text-[18px] md:text-[24px]">BUT Informatique</p>
+                            <p className="poppins font-medium text-[16px] md:text-[20px] text-neutral-400">Université de Bordeaux (33)</p>
+                            <p className="poppins font-medium text-[16px] md:text-[20px] text-neutral-400">2021 - Aujourd'hui</p>
                         </div>
-                        <div className="h-[100px]"></div>
+                        <div className="h-[70px] md:h-[100px]"></div>
                         <div className="flex flex-col ms-5">
-                            <p className="poppins font-semibold text-[24px]">Bac Général</p>
-                            <p className="poppins font-medium text-[20px] text-neutral-400">Lycée Fernand Daguin (33)</p>
-                            <p className="poppins font-medium text-[20px] text-neutral-400">2018 - 2021</p>
-                            <p className="poppins font-medium text-[20px] text-neutral-400">NSI - Histoire des Arts - Maths</p>
-                            <p className="poppins font-medium text-[20px] text-neutral-400">Section Européenne Anglais</p>
+                            <p className="poppins font-semibold text-[18px] md:text-[24px]">Bac Général</p>
+                            <p className="poppins font-medium text-[16px] md:text-[20px] text-neutral-400">Lycée Fernand Daguin (33)</p>
+                            <p className="poppins font-medium text-[16px] md:text-[20px] text-neutral-400">2018 - 2021</p>
+                            <p className="poppins font-medium text-[16px] md:text-[20px] text-neutral-400">NSI - Histoire des Arts - Maths</p>
+                            <p className="poppins font-medium text-[16px] md:text-[20px] text-neutral-400">Section Européenne Anglais</p>
                         </div>
                     </div>
                 </div>
